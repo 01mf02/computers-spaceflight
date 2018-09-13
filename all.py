2355dcce54a08e0a1524d302fc13193c6a988de7
Michael Färber <01mf02@gmail.com>
2018-09-13 18:31:08 +0200

    Use KOMA-Script's \addchap and \addpart commands.

--- all.py
+++ all.py
@@ -1,6 +1,7 @@
 from pfcompat import toJSONFilters
 from pandoc_latex_environment import environment
 from figure import figure
+from unnumbered import unnumbered
 
 if __name__ == "__main__":
-    toJSONFilters([environment, figure])
+    toJSONFilters([environment, figure, unnumbered])
---
+++ unnumbered.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# debugging
+from __future__ import print_function
+import sys
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+from pandocfilters import toJSONFilter, RawInline, Para
+from pfcompat import Image
+
+def latex(x):
+    return RawInline('latex', x)
+
+def unnumbered(key, value, format, meta):
+    #eprint("Key: " + key)
+    if key == 'Header':
+        [level, [ident, classes, kvs], inlines] = value
+        eprint(inlines)
+        if level <= 2 and 'unnumbered' in classes:
+            cmd = 'addpart' if level == 1 else 'addchap'
+            return \
+                Para([ latex("\\" + cmd + '{') ] + inlines + [ latex('}\n')])
+
+if __name__ == "__main__":
+    toJSONFilter(unnumbered)
